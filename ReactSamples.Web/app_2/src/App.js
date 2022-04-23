95de4211f87418aa9500b28dbb52d80bfa3350d9
Zenno <zenno.neagu@gmail.com>
2022-04-24 00:07:15 +0100

    introduced React Portal for modal pop!

--- ReactSamples.Web/app_2/src/App.js
+++ ReactSamples.Web/app_2/src/App.js
@@ -1,74 +1,12 @@
-import React, { useReducer } from "react";
 import './App.css';
+import BubblesContainer from './components/BubblesContainer';
 
 function App() {
 
-    const initialBaloonsState = { red: 100, blue: 100, orange: 100, green: 100 };
-
-    function reducer(state, action) {
-        switch (action.type) {
-            case 'inc_red':
-                state.red += 10;
-                break;
-            case 'inc_blue':
-                state.blue += 10;
-                break;
-            case 'inc_green':
-                state.green += 10;
-                break;
-            case 'inc_orange':
-                state.orange += 10;
-                break;
-
-            case 'dec_red':
-                state.red = 50;
-                break;
-            case 'dec_blue':
-                state.blue = 50;
-                break;
-            case 'dec_green':
-                state.green = 50;
-                break;
-            case 'dec_orange':
-                state.orange = 50;
-                break;
-        }
-
-        return { ...state };
-    }
-
-    const [state, dispatch] = useReducer(reducer, initialBaloonsState);
-
-
     return <div>
-
-        <div className="ac_18075_0895 pos_18075_0927">
-            <div className="ac_18075_6943 pos_18075_0929">Handle state with Redux</div>
-            <img src="https://webdesignsurface.com:443/_data/9112/17058/b580ebc008.png" style={{ width: state.red + "px" }}
-                className="ac_18075_5757 ac_18075_3525 pos_18075_0931" onClick={() => dispatch({ type: 'dec_red' })} />
-            <img src="https://webdesignsurface.com:443/_data/9112/17058/b580ebc008.png" style={{ width: state.blue + "px" }}
-                className="ac_18075_5757 ac_18075_6901 pos_18075_0933" onClick={() => dispatch({ type: 'dec_blue' })} />
-            <img src="https://webdesignsurface.com:443/_data/9112/17058/b580ebc008.png" style={{ width: state.green + "px" }}
-                className="ac_18075_5757 ac_18075_0855  pos_18075_0935" onClick={() => dispatch({ type: 'dec_green' })} />
-            <img src="https://webdesignsurface.com:443/_data/9112/17058/b580ebc008.png" style={{ width: state.orange + "px" }}
-                className="ac_18075_5757 ac_18075_3949 pos_18075_0937" onClick={() => dispatch({ type: 'dec_orange' })} />
-        </div>
-
-        <div>
-            <div className="ac_18075_2900 ac_18075_7267 pos_18075_0906" onClick={() => dispatch({ type: 'inc_red' })}></div>
-            <div className="ac_18075_7267 ac_18075_9605 pos_18075_0909" onClick={() => dispatch({ type: 'inc_blue' })}></div>
-            <div className="ac_18075_7267 ac_18075_6556 pos_18075_0918" onClick={() => dispatch({ type: 'inc_green' })}></div>
-            <div className="ac_18075_7267 ac_18075_5389 pos_18075_0923" onClick={() => dispatch({ type: 'inc_orange' })}></div>
-        </div>
-
-        <div className="pos_18075_0969">
-            <a className="ac_18064_7722 pos_18075_0972" href="https://react.cyberzenno.com/" target="_blank" rel="noreferrer" >React Samples</a>
-            <a href="https://webdesignsurface.com/surface/view/18075/react-course_app_2---redux-sample" className="ac_18064_7722 pos_18075_0975" target="_blank" rel="noreferrer" >Edit App UI</a>
-            <a href="https://github.com/cyberzenno/react-samples" className="ac_18064_7722  pos_18075_0978" target="_blank" rel="noreferrer" >Source Code on GitHub</a>
-        </div>
-
-
+        <BubblesContainer />
     </div>
+
 }
 
 export default App;
---
+++ ReactSamples.Web/app_2/src/components/PopModal.js
@@ -0,0 +1,30 @@
+﻿import ReactDOM from "react-dom"
+
+function Pop(props) {
+
+    function getRandomInt(min, max) {
+        
+        var value = Math.random() * (max - min) + min;
+        return value;
+    }
+
+    var top = getRandomInt(10, 200);
+    var left = getRandomInt(300, 500);
+    var rotate = getRandomInt(-30, 60)
+
+    return <div style={{ transform: 'rotate(' + rotate + 'deg)', position: 'absolute', top: top + 'px', left: left + 'px' }} className={"ac_18075_4448 " + props.colorPopped}>Pop!</div>
+}
+
+function PopModal(props) {
+
+    setTimeout(function () {
+
+        props.dispatch({ type: 'close_pop' })
+
+    }, 400);
+
+    var popModalRoot = document.getElementById("pop-modal-root");
+    return ReactDOM.createPortal(<Pop colorPopped={props.colorPopped} />, popModalRoot)
+}
+
+export default PopModal;
